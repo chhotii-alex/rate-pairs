cd08b31af2435feddc65b9e5520a69ddc35ecdcb
Alex Morgan <lexyym@gmail.com>
2024-12-25 10:32:06 -0500

    some actual  data

--- src/routes/data.js
+++ src/routes/data.js
@@ -1,4 +1,7 @@
 let pairs = [
+	['1-1-flip-color.png', '1--flip-color.png'],
+	['1--rotated.png', '1-1-rotated.png'],
+	['1-.png', '1-1.png'],
 	['backhoe.jpg', 'bald_eagle.jpg'],
 	['baluster.jpg', 'barrel_vault.jpg'],
 	['baseball.jpg', 'bathroom.jpg'],
